09deaab608c2efd6450209723e271d9c09b38a88
glenhermanus <93304207+glenhermanus@users.noreply.github.com>
2021-10-28 11:22:20 +0700

    Add files via upload

---
+++ lib/constant/api_path.dart
@@ -0,0 +1,2 @@
+const BASE_URL =
+    'https://backend-service-development-dot-mogawe-222614.appspot.com/';
---
+++ lib/constant/app_const_value.dart
@@ -0,0 +1,6 @@
+//VERSION STATE
+const DEPRECATED_VERSION = "deprecate version";
+const OLD_VERSION = "old version";
+const LATEST_VERSION = "latest version";
+
+const UPDATE_AVAILABLE = "pref.update_avaliable";
