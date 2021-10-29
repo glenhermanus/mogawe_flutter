a15e4e949123cc0975c22e567cec111dcf32868c
Singgih Putra <singgihputrap@gmail.com>
2021-10-29 19:07:07 +0700

    add bloc

---
+++ lib/blocs/reset_password/bloc/resetpassword_bloc.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:mogawe/core/data/request/reset_password_request.dart';
+import 'package:mogawe/core/data/response/reset_password_response.dart';
+import 'package:mogawe/modules/auth/repositories/auth_repository.dart';
+
+part 'resetpassword_event.dart';
+part 'resetpassword_state.dart';
+
+class ResetpasswordBloc extends Bloc<ResetpasswordEvent, ResetpasswordState> {
+  final _repository = AuthRepository();
+
+  ResetpasswordBloc() : super(ResetpasswordInitial()) {
+    on<ResetpasswordSubmitted>(_onResetpasswordSubmitted);
+  }
+
+  FutureOr<void> _onResetpasswordSubmitted(
+      ResetpasswordSubmitted event, Emitter<ResetpasswordState> emit) async {
+    try {
+      emit(ResetpasswordLoading());
+      final response = await _repository.resetPassword(event.request);
+      emit(ResetpasswordLoaded(response));
+    } catch (e) {
+      print(e);
+    }
+  }
+}
