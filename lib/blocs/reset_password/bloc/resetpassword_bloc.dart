5303d1fb03c969a8788b7f83113af892552a0b20
Singgih Putra <singgihputrap@gmail.com>
2021-10-30 11:29:48 +0700

    add sha256

--- lib/blocs/reset_password/bloc/resetpassword_bloc.dart
+++ lib/blocs/reset_password/bloc/resetpassword_bloc.dart
@@ -5,6 +5,7 @@ import 'package:meta/meta.dart';
 import 'package:mogawe/core/data/request/reset_password_request.dart';
 import 'package:mogawe/core/data/response/reset_password_response.dart';
 import 'package:mogawe/modules/auth/repositories/auth_repository.dart';
+import 'package:mogawe/utils/services/password_hasher.dart';
 
 part 'resetpassword_event.dart';
 part 'resetpassword_state.dart';
@@ -20,6 +21,8 @@ class ResetpasswordBloc extends Bloc<ResetpasswordEvent, ResetpasswordState> {
       ResetpasswordSubmitted event, Emitter<ResetpasswordState> emit) async {
     try {
       emit(ResetpasswordLoading());
+      event.request.newPassword =
+          PasswordHasher().convertToSha256(event.request.newPassword ?? '');
       final response = await _repository.resetPassword(event.request);
       emit(ResetpasswordLoaded(response));
     } catch (e) {
